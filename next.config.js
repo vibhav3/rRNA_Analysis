26a03f2ae060121686df0fe235cbda34717e955e
Vibhav Iyengar <vibhavi@utexas.edu>
2025-10-27 15:17:26 -0500

    v2

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // add any other settings you had in next.config.ts here
+};
+
+module.exports = nextConfig;
+
